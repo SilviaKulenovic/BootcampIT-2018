13958189bc589f25284aa66b9f1b8a4e8ec70173
SilviaKulenovic <45363266+SilviaKulenovic@users.noreply.github.com>
2018-12-07 15:36:37 +0100

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,42 @@
+
 #include <iostream>
+#include<math.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello Hello world!!!" <<  endl;
-	system("PAUSE");
+	int n = 0;
+	int x = 0;
+	int cijena = 0;
+	int p1, p2, p3 = 0;
+	int i = 0;
+	cin >> n;
+	cin >> x;
+
+
+	for (int i=0;i<n;i++){
+	    cin >> p1;
+	    cin >> p2;
+		int j = abs(p1 - p2);
+		   if (j <= x) {
+
+			    if (p1 > p2) {
+			    cijena +=p1;
+		        }
+				else {
+					cijena += p2;
+				}
+		   }
+		   else   {
+		     cin >> p3;
+			cijena += p3;
+		
+		   }
+	}
+	cout << cijena;
+    
+    
+
 	return 0;
 }
